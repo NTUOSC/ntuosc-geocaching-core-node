6a20327e5706bace9ee2348e3d294765a0cb0400
Karinsu <m80126colin@gmail.com>
2016-09-05 00:19:30 +0800

    Update config files

---
+++ config/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  database: {
+    dir:  './path/to/database',
+    file: './path/to/database/database.json',
+    init: {}
+  },
+  ssl: {
+    key:  './your/private/key.pem',
+    cert: './your/certificate.pem'
+  },
+  port: 5566
+}
